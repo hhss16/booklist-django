e41a7152ca4213ae02efb7707621918f3c078495
Hasin Hayder <hasin@leevio.com>
2022-09-23 20:34:17 +0600

    booklist

---
+++ booksapi/views.py
@@ -0,0 +1,50 @@
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+from .models import Book
+from django.views.decorators.csrf import csrf_exempt
+from django.core.serializers import serialize
+from django.forms.models import model_to_dict
+from django.http import QueryDict
+# Create your views here.
+
+@csrf_exempt
+def books(request):
+    if request.method == 'GET':
+        books = Book.objects.all().values()
+        return JsonResponse({"books":list(books)})
+    elif request.method == 'POST':
+        title = request.POST.get('title');
+        author = request.POST.get('author');
+        price = request.POST.get('price');
+        book = Book(
+            title = title,
+            author = author,
+            price = price
+        )
+        book.save()
+        # data = serialize("json", book)
+        # return HttpResponse(data, content_type="application/json")
+        return JsonResponse(model_to_dict(book))
+
+@csrf_exempt
+def book(request, pk):
+    if request.method == 'GET':
+        book = Book.objects.get(pk=pk)
+        return JsonResponse(model_to_dict(book))
+    elif request.method == 'PUT':
+        book = Book.objects.get(pk=pk)
+        reqbody = QueryDict(request.body)
+        title = reqbody.get('title');
+        author = reqbody.get('author');
+        price = reqbody.get('price');
+        if title:
+            book.title = title
+        if author:
+            book.author = author
+        if price:
+            book.price = price 
+
+        book.save()
+        
+        return JsonResponse(model_to_dict(book))
+
